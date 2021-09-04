0f43d9a9b57c90b391e420dddf29ab6a8ad2705a
Calvin Kim <calvin@kcalvinalvin.info>
2021-09-04 16:16:55 +0900

    btcd: Add trace profile flag
    Trace helps diagnose initial block download slowdowns.  Shouldn't really
    be turned on for regular use as it's very expensive.

--- btcd.go
+++ btcd.go
@@ -14,6 +14,7 @@ import (
 	"runtime"
 	"runtime/debug"
 	"runtime/pprof"
+	"runtime/trace"
 
 	"github.com/btcsuite/btcd/blockchain/indexers"
 	"github.com/btcsuite/btcd/database"
@@ -96,6 +97,18 @@ func btcdMain(serverChan chan<- *server) error {
 		}
 		defer f.Close()
 		defer pprof.WriteHeapProfile(f)
+	}
+
+	// Write trace profile if requested.
+	if cfg.TraceProfile != "" {
+		f, err := os.Create(cfg.TraceProfile)
+		if err != nil {
+			btcdLog.Errorf("Unable to create trace profile: %v", err)
+			return err
+		}
+		trace.Start(f)
+		defer f.Close()
+		defer trace.Stop()
 	}
 
 	// Perform upgrades to btcd as new versions require it.
