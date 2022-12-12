a38d106579dc7ab9bb5705c5a10482cba9729f12
Calvin Kim <calvin@kcalvinalvin.info>
2022-12-12 15:55:45 +0900

    wire: Fix segwit size bug

--- wire/msgtx.go
+++ wire/msgtx.go
@@ -101,13 +101,13 @@ const (
 	// item: 1 byte for length + 1 byte for the witness item itself, or two
 	// bytes. This value is then divided by the currently allowed maximum
 	// "cost" for a transaction.
-	maxWitnessItemsPerInput = 500000
+	maxWitnessItemsPerInput = 4_000_000
 
 	// maxWitnessItemSize is the maximum allowed size for an item within
 	// an input's witness data. This number is derived from the fact that
 	// for script validation, each pushed item onto the stack must be less
 	// than 10k bytes.
-	maxWitnessItemSize = 11000
+	maxWitnessItemSize = 4_000_000
 )
 
 // TxFlagMarker is the first byte of the FLAG field in a bitcoin tx
