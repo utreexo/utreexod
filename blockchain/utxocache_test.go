b3abff6df6584e1c200b446593cf7b7f3d102acc
Calvin Kim <calvin@kcalvinalvin.info>
2023-10-13 16:18:02 +0900

    fixup! blockchain: Add mapslice

--- blockchain/utxocache_test.go
+++ blockchain/utxocache_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 
@@ -82,6 +83,93 @@ func TestMapSlice(t *testing.T) {
 				t.Fatalf("expected value of %v, got %v", expected, got)
 			}
 		}
+	}
+}
+
+// TestMapsliceConcurrency just tests that the mapslice won't result in a panic
+// on concurrent access.
+func TestMapsliceConcurrency(t *testing.T) {
+	tests := []struct {
+		keys []wire.OutPoint
+	}{
+		{
+			keys: func() []wire.OutPoint {
+				outPoints := make([]wire.OutPoint, 10000)
+				for i := uint32(0); i < uint32(len(outPoints)); i++ {
+					var buf [4]byte
+					binary.BigEndian.PutUint32(buf[:], i)
+					hash := sha256.Sum256(buf[:])
+
+					op := wire.OutPoint{Hash: hash, Index: i}
+					outPoints[i] = op
+				}
+				return outPoints
+			}(),
+		},
+	}
+
+	for _, test := range tests {
+		maxSize := calculateRoughMapSize(1000, bucketSize)
+
+		maxEntriesFirstMap := 500
+		ms1 := make(map[wire.OutPoint]*UtxoEntry, maxEntriesFirstMap)
+		ms := mapSlice{
+			maps:                []map[wire.OutPoint]*UtxoEntry{ms1},
+			maxEntries:          []int{maxEntriesFirstMap},
+			maxTotalMemoryUsage: uint64(maxSize),
+		}
+
+		var wg sync.WaitGroup
+
+		wg.Add(1)
+		go func(m *mapSlice, keys []wire.OutPoint) {
+			defer wg.Done()
+			for i := 0; i < 5000; i++ {
+				m.put(keys[i], nil, 0)
+			}
+		}(&ms, test.keys)
+
+		wg.Add(1)
+		go func(m *mapSlice, keys []wire.OutPoint) {
+			defer wg.Done()
+			for i := 5000; i < 10000; i++ {
+				m.put(keys[i], nil, 0)
+			}
+		}(&ms, test.keys)
+
+		wg.Add(1)
+		go func(m *mapSlice) {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				m.size()
+			}
+		}(&ms)
+
+		wg.Add(1)
+		go func(m *mapSlice) {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				m.length()
+			}
+		}(&ms)
+
+		wg.Add(1)
+		go func(m *mapSlice, keys []wire.OutPoint) {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				m.get(keys[i])
+			}
+		}(&ms, test.keys)
+
+		wg.Add(1)
+		go func(m *mapSlice, keys []wire.OutPoint) {
+			defer wg.Done()
+			for i := 0; i < 5000; i++ {
+				m.delete(keys[i])
+			}
+		}(&ms, test.keys)
+
+		wg.Wait()
 	}
 }
 
