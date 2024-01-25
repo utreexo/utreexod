e6462ab57dc8cdee07f18b494b7e485c03e87421
志宇 <hello@evanlinjin.me>
2024-01-25 18:26:19 +0900

    main: add install cmd to rust build script

--- buildbdkgo.sh
+++ buildbdkgo.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cargo build --release && uniffi-bindgen-go -o bdkwallet bdkwallet/bdkgo_crate/src/bdkgo.udl
+cargo install uniffi-bindgen-go --git https://github.com/NordSecurity/uniffi-bindgen-go --tag v0.2.0+v0.25.0 && \
+cargo build --release && \
+uniffi-bindgen-go -o bdkwallet bdkwallet/bdkgo_crate/src/bdkgo.udl
