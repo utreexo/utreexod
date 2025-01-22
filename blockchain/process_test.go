181977c5312e79703ea10d0e191226836c752902
Calvin Kim <calvin@kcalvinalvin.info>
2025-01-22 16:21:05 +0900

    blockchain: add chain selection logic to maybeAcceptBlockHeader
    maybeAcceptBlockHeader was missing the chain selection logic that would
    be needed for handling block annoucements for ones that don't extend the
    current chain tip.
    
    It's now able to determine if a block header extends the chain tip or
    not and returns a boolean indicating if the given header is on the
    best chain of headers.

--- blockchain/process_test.go
+++ blockchain/process_test.go
@@ -58,6 +58,9 @@ func TestProcessBlockHeader(t *testing.T) {
 	chain, params, tearDown := utxoCacheTestChain("TestProcessBlockHeader")
 	defer tearDown()
 
+	// Generate and process the intial 10 block headers.
+	//
+	// 	genesis -> 1  -> 2  -> ...  -> 10 (active)
 	headers := chainedHeaders(&params.GenesisBlock.Header, params, 0, 10)
 	for _, header := range headers {
 		err := chain.ProcessBlockHeader(header, BFNone)
@@ -66,14 +69,33 @@ func TestProcessBlockHeader(t *testing.T) {
 		}
 	}
 
+	// Check that the tip is correct.
+	lastHeader := headers[len(headers)-1]
+	lastHeaderHash := lastHeader.BlockHash()
+	tipNode := chain.bestHeader.Tip()
+	if !tipNode.hash.IsEqual(&lastHeaderHash) {
+		t.Fatalf("expected %v but got %v", lastHeaderHash.String(), tipNode.hash.String())
+	}
+	if tipNode.status != statusNone {
+		t.Fatalf("expected statusNone but got %v", tipNode.status)
+	}
+	if tipNode.height != int32(len(headers)) {
+		t.Fatalf("expected height of %v but got %v", len(headers), tipNode.height)
+	}
+
 	// Create sidechain block headers.
+	//
+	// 	genesis -> 1  -> 2  -> 3  -> 4  -> 5  -> ... -> 10 (active)
+	//                                            \-> 6  -> ... -> 8 (valid-fork)
 	blockHash := headers[4].BlockHash()
 	node := chain.IndexLookupNode(&blockHash)
+	forkHeight := node.height
 	sideChainHeaders := chainedHeaders(headers[4], params, node.height, 3)
+	sidechainTip := sideChainHeaders[len(sideChainHeaders)-1]
 
 	// Test that the last block header fails as it's missing the previous block
 	// header.
-	err := chain.ProcessBlockHeader(sideChainHeaders[len(sideChainHeaders)-1], BFNone)
+	err := chain.ProcessBlockHeader(sidechainTip, BFNone)
 	if err == nil {
 		err := fmt.Errorf("sideChainHeader %v passed verification but "+
 			"should've failed verification"+
@@ -89,5 +111,58 @@ func TestProcessBlockHeader(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
+	}
+
+	// Check that the tip is still the same as before.
+	if !tipNode.hash.IsEqual(&lastHeaderHash) {
+		t.Fatalf("expected %v but got %v", lastHeaderHash.String(), tipNode.hash.String())
+	}
+	if tipNode.status != statusNone {
+		t.Fatalf("expected statusNone but got %v", tipNode.status)
+	}
+	if tipNode.height != int32(len(headers)) {
+		t.Fatalf("expected height of %v but got %v", len(headers), tipNode.height)
+	}
+
+	// Verify that the side-chain extending headers verify.
+	sidechainExtendingHeaders := chainedHeaders(sidechainTip, params, forkHeight+int32(len(sideChainHeaders)), 10)
+	for _, header := range sidechainExtendingHeaders {
+		err := chain.ProcessBlockHeader(header, BFNone)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Create more sidechain block headers so that it becomes the active chain.
+	//
+	// 	genesis -> 1  -> 2  -> 3  -> 4  -> 5  -> ... -> 10 (valid-fork)
+	//                                            \-> 6  -> ... -> 18 (active)
+	lastSidechainHeader := sidechainExtendingHeaders[len(sidechainExtendingHeaders)-1]
+	lastSidechainHeaderHash := lastSidechainHeader.BlockHash()
+
+	// Check that the tip is now different.
+	tipNode = chain.bestHeader.Tip()
+	if !tipNode.hash.IsEqual(&lastSidechainHeaderHash) {
+		t.Fatalf("expected %v but got %v", lastSidechainHeaderHash.String(), tipNode.hash.String())
+	}
+	if tipNode.status != statusNone {
+		t.Fatalf("expected statusNone but got %v", tipNode.status)
+	}
+	if tipNode.height != int32(len(sideChainHeaders)+len(sidechainExtendingHeaders))+forkHeight {
+		t.Fatalf("expected height of %v but got %v", len(headers), tipNode.height)
+	}
+
+	// Extend the original headers and check it still verifies.
+	extendedOrigHeaders := chainedHeaders(lastHeader, params, int32(len(headers)), 2)
+	for _, header := range extendedOrigHeaders {
+		err := chain.ProcessBlockHeader(header, BFNone)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Check that the tip didn't change.
+	if !tipNode.hash.IsEqual(&lastSidechainHeaderHash) {
+		t.Fatalf("expected %v but got %v", lastSidechainHeaderHash.String(), tipNode.hash.String())
 	}
 }
