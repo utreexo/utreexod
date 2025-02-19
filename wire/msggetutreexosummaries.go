96c91ba6b84b186050ee3349044012378bb02b07
Calvin Kim <calvin@kcalvinalvin.info>
2025-02-19 15:56:41 +0900

    wire: change msggetutreexosummaries structure
    The structure is changed so that the caller can ask for multiple
    summaries at once.

--- wire/msggetutreexosummaries.go
+++ wire/msggetutreexosummaries.go
@@ -4,48 +4,35 @@
 package wire
 
 import (
+	"encoding/binary"
 	"io"
-
-	"github.com/utreexo/utreexod/chaincfg/chainhash"
 )
 
 // MsgGetUtreexoSummaries implements the Message interface and represents a bitcoin
-// getutreexoheader message. It's used to request the utreexo header at the given
-// block.
+// getutreexosummaries message. It's used to request the utreexo summaries at the given
+// blocks.
 type MsgGetUtreexoSummaries struct {
-	BlockHash    chainhash.Hash
-	IncludeProof bool
+	BlockPosition uint64
 }
 
 // BtcDecode decodes r using the bitcoin protocol encoding into the receiver.
 // This is part of the Message interface implementation.
 func (msg *MsgGetUtreexoSummaries) BtcDecode(r io.Reader, pver uint32, enc MessageEncoding) error {
-	_, err := io.ReadFull(r, msg.BlockHash[:])
-	if err != nil {
-		return err
-	}
+	bs := newSerializer()
+	defer bs.free()
 
-	msg.IncludeProof = msg.BlockHash[31] == 2
-	msg.BlockHash[31] = 0
-
-	return nil
+	var err error
+	msg.BlockPosition, err = bs.Uint64(r, binary.LittleEndian)
+	return err
 }
 
 // BtcEncode encodes the receiver to w using the bitcoin protocol encoding.
 // This is part of the Message interface implementation.
 func (msg *MsgGetUtreexoSummaries) BtcEncode(w io.Writer, pver uint32, enc MessageEncoding) error {
-	if !msg.IncludeProof {
-		msg.BlockHash[31] = 1
-	} else {
-		msg.BlockHash[31] = 2
-	}
+	bs := newSerializer()
+	defer bs.free()
 
-	_, err := w.Write(msg.BlockHash[:])
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return bs.PutUint64(w, binary.LittleEndian, msg.BlockPosition)
 }
 
 // Command returns the protocol command string for the message.  This is part
@@ -57,11 +44,11 @@ func (msg *MsgGetUtreexoSummaries) Command() string {
 // MaxPayloadLength returns the maximum length the payload can be for the
 // receiver.  This is part of the Message interface implementation.
 func (msg *MsgGetUtreexoSummaries) MaxPayloadLength(pver uint32) uint32 {
-	return chainhash.HashSize
+	return 8
 }
 
-// NewMsgGetUtreexoSummaries returns a new bitcoin getheaders message that conforms to
+// NewMsgGetUtreexoSummaries returns a new bitcoin getutreexosummaries message that conforms to
 // the Message interface.  See MsgGetUtreexoSummaries for details.
-func NewMsgGetUtreexoSummaries(blockHash chainhash.Hash, includeProof bool) *MsgGetUtreexoSummaries {
-	return &MsgGetUtreexoSummaries{BlockHash: blockHash, IncludeProof: includeProof}
+func NewMsgGetUtreexoSummaries(blockPosition uint64) *MsgGetUtreexoSummaries {
+	return &MsgGetUtreexoSummaries{BlockPosition: blockPosition}
 }
