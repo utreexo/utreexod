8a57aef7cfc93321d4ee996112ff30e582f5e6d9
Calvin Kim <calvin@kcalvinalvin.info>
2025-08-25 18:00:42 +0900

    bdkgo: update the bdkgo files
    We update the autogenerated files from uniffi.

--- bdkwallet/bdkgo/bdkgo.c
+++
@@ -1,8 +0,0 @@
-#include <bdkgo.h>
-
-// This file exists beacause of
-// https://github.com/golang/go/issues/11263
-
-void cgo_rust_task_callback_bridge_bdkgo(RustTaskCallback cb, const void * taskData, int8_t status) {
-  cb(taskData, status);
-}
