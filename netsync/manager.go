fa29397bf01a58d482bacd05acd17a3682589eb7
Calvin Kim <calvin@kcalvinalvin.info>
2026-01-04 19:31:39 +0900

    netsync: add check to verify that the aggregator is 0
    When the swiftsync part of ibd is complete, the aggregator must be at 0.
    We now perform this check.

--- netsync/manager.go
+++ netsync/manager.go
@@ -975,6 +975,22 @@ func (sm *SyncManager) handleBlockMsg(bmsg *blockMsg) {
 		}
 	}
 
+	// If we're at the block when the swiftsync part of ibd ends, make sure the
+	// aggregator is zero.
+	if sm.committedTTLAcc != nil &&
+		bmsg.block.Height() == int32(sm.committedTTLAcc.NumLeaves-1) {
+		if !sm.chain.IsAggZero() {
+			log.Warnf("The swiftsync aggregator is non-zero at swiftsync checkpoint at block %v(%v). "+
+				"The binary is likely corrupted and the user should not trust this "+
+				"software as genuine and there may be attempts to steal funds. The user "+
+				"should delete the datadir and the binary from their computer.",
+				bmsg.block.Hash(), bmsg.block.Height())
+			os.Exit(1)
+		}
+
+		log.Infof("Verified swiftsync checkpoint block at %v(%v)", bmsg.block.Hash(), bmsg.block.Height())
+	}
+
 	// If we are not in headers first mode, it's a good time to periodically
 	// flush the blockchain cache because we don't expect new blocks immediately.
 	// After that, there is nothing more to do.
