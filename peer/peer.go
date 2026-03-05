a4ab4c468b65e1e4f1ca798255b0a84a42d5e67b
Olaoluwa Osuntokun <laolu32@gmail.com>
2026-03-05 22:23:12 +0900

    rpcserver: expose v2 connection status in getpeerinfo
    This commit adds a new boolean field, `V2Connection`, to the
    `getpeerinfo` RPC result. This field indicates whether the connection
    to the peer is using the v2 encrypted transport protocol.
    
    The `peer.StatsSnap` struct is updated to include this `V2Connection`
    field, which is populated based on the `UsingV2Conn` field in the peer's
    configuration. The `btcjson.GetPeerInfoResult` struct is also updated
    to include the corresponding JSON field `v2_connection`. Finally, the
    `handleGetPeerInfo` RPC handler is modified to copy this value from the
    peer's stats snapshot into the RPC response.

--- peer/peer.go
+++ peer/peer.go
@@ -404,6 +404,7 @@ type StatsSnap struct {
 	LastPingNonce  uint64
 	LastPingTime   time.Time
 	LastPingMicros int64
+	V2Connection   bool
 }
 
 // HashFunc is a function which returns a block hash, height and error
@@ -586,6 +587,7 @@ func (p *Peer) StatsSnapshot() *StatsSnap {
 		LastPingNonce:  p.lastPingNonce,
 		LastPingMicros: p.lastPingMicros,
 		LastPingTime:   p.lastPingTime,
+		V2Connection:   p.cfg.UsingV2Conn,
 	}
 
 	p.statsMtx.RUnlock()
