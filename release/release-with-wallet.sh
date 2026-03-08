0467a8f04c7555c825e57ca6a8f0f130acdbfc4e
Calvin Kim <calvin@kcalvinalvin.info>
2026-03-09 01:06:46 +0900

    release: improve wallet build cross-compilation with FreeBSD support
    - Add REPOROOT variable so cargo/go find source files after cd
    - Set CARGO_TARGET_<TRIPLE>_LINKER and CC_<triple> env vars for each
      target so cargo's cc crate can cross-compile vendored C code
    - Disable _FORTIFY_SOURCE for musl targets to avoid missing symbols
    - Fix ARM version checks ($ARM=7 -> $ARM = 7)
    - Fix armv6 Rust target triple (armv6 -> arm)
    - Build cargo with --manifest-path pointing to repo root
    - Copy libbdkgo.a to target/release/ for CGO to find
    - Add separate go build branches for linux, darwin, windows, freebsd
    - Add freebsd-amd64 to default Linux build targets
    - Clean up cargo build cache after all targets are built

--- release/release-with-wallet.sh
+++ release/release-with-wallet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # Copyright (c) 2016 Company 0, LLC.
 # Copyright (c) 2016-2020 The btcsuite developers
 # Use of this source code is governed by an ISC
@@ -34,6 +36,9 @@ PACKAGESRC="$MAINDIR/$PACKAGE-source-$TAG.tar"
 git archive -o $PACKAGESRC HEAD
 gzip -f $PACKAGESRC > "$PACKAGESRC.gz"
 
+# Save repo root so cargo and go can find source files after we cd.
+REPOROOT="$(git rev-parse --show-toplevel)"
+
 cd $MAINDIR
 
 SYS=
@@ -47,6 +52,7 @@ if [[ $(uname) == Linux ]]; then
           linux-arm64
           linux-amd64
           windows-amd64
+          freebsd-amd64
   "}
 elif [[ $(uname) == Darwin ]]; then
   # If BTCDBUILDSYS is set the default list is ignored. Useful to release
@@ -64,16 +70,12 @@ elif [[ $(uname) == Darwin ]]; then
   CPUCORES=$(sysctl -n hw.physicalcpu)
 fi
 
-# Use the first element of $GOPATH in the case where GOPATH is a list
-# (something that is totally allowed).
-PKG="github.com/utreexo/utreexod"
-COMMIT=$(git describe --tags --abbrev=40 --dirty)
-
 for i in $SYS; do
     OS=$(echo $i | cut -f1 -d-)
     ARCH=$(echo $i | cut -f2 -d-)
     ARM=
     CC=
+    CARGO_ENV=
 
     if [[ $ARCH = "armv6" ]]; then
       ARCH=arm
@@ -87,49 +89,107 @@ for i in $SYS; do
     echo "cd to" $PACKAGE-$i-$TAG
     cd $PACKAGE-$i-$TAG
 
-    # Build bdk wallet(rust) dependency.
+    # Set the Rust target triple, the C cross-compiler (CC), and cargo
+    # environment variables.  Cargo needs two things for cross-compilation:
+    #   CARGO_TARGET_<TRIPLE>_LINKER  – which linker to invoke
+    #   CC_<triple>                   – which C compiler the `cc` crate uses
+    #                                   to compile vendored C code (e.g. secp256k1)
+    # CFLAGS_<triple> disables _FORTIFY_SOURCE which is incompatible with musl
+    # static linking (some musl cross-compilers ship fortify-headers that
+    # reference symbols like __memcpy_chk which musl libc does not provide).
     if [[ $ARCH = "arm64" ]] && [[ $OS = "linux" ]]; then
       TARGET="aarch64-unknown-linux-musl"
       CC=aarch64-linux-musl-gcc
+      CARGO_ENV="CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER=aarch64-linux-musl-gcc CC_aarch64_unknown_linux_musl=aarch64-linux-musl-gcc CFLAGS_aarch64_unknown_linux_musl=-U_FORTIFY_SOURCE"
 
     elif [[ $ARCH = "amd64" ]] && [[ $OS = "linux" ]]; then
       TARGET="x86_64-unknown-linux-musl"
       CC=x86_64-linux-musl-gcc
+      CARGO_ENV="CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER=x86_64-linux-musl-gcc CC_x86_64_unknown_linux_musl=x86_64-linux-musl-gcc CFLAGS_x86_64_unknown_linux_musl=-U_FORTIFY_SOURCE"
 
-    elif [[ $ARCH = "arm" ]] && [[ $OS = "linux" ]] && [[ $ARM=7 ]]; then
+    elif [[ $ARCH = "arm" ]] && [[ $OS = "linux" ]] && [[ $ARM = 7 ]]; then
       TARGET="armv7-unknown-linux-musleabihf"
       CC=armv7l-linux-musleabihf-gcc
+      CARGO_ENV="CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER=armv7l-linux-musleabihf-gcc CC_armv7_unknown_linux_musleabihf=armv7l-linux-musleabihf-gcc CFLAGS_armv7_unknown_linux_musleabihf=-U_FORTIFY_SOURCE"
 
-    elif [[ $ARCH = "arm" ]] && [[ $OS = "linux" ]] && [[ $ARM=6 ]]; then
-      TARGET="armv6-unknown-linux-musleabihf"
+    elif [[ $ARCH = "arm" ]] && [[ $OS = "linux" ]] && [[ $ARM = 6 ]]; then
+      TARGET="arm-unknown-linux-musleabihf"
       CC=armv6-linux-musleabihf-gcc
+      CARGO_ENV="CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSLEABIHF_LINKER=armv6-linux-musleabihf-gcc CC_arm_unknown_linux_musleabihf=armv6-linux-musleabihf-gcc CFLAGS_arm_unknown_linux_musleabihf=-U_FORTIFY_SOURCE"
 
     elif [[ $ARCH = "arm64" ]] && [[ $OS = "darwin" ]]; then
       TARGET="aarch64-apple-darwin"
+      # macOS builds use the system Xcode toolchain (not Nix) for universal
+      # binary support. Cross-compiling requires the SDK with universal libs.
+      SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+      if [[ $(uname -m) != arm64 ]]; then
+        CC="clang -arch arm64 -isysroot $SDKROOT"
+      fi
 
     elif [[ $ARCH = "amd64" ]] && [[ $OS = "darwin" ]]; then
       TARGET="x86_64-apple-darwin"
+      # macOS builds use the system Xcode toolchain (not Nix) for universal
+      # binary support. Cross-compiling requires the SDK with universal libs.
+      SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+      if [[ $(uname -m) != x86_64 ]]; then
+        CC="clang -arch x86_64 -isysroot $SDKROOT"
+      fi
 
     elif [[ $ARCH = "amd64" ]] && [[ $OS = "windows" ]]; then
       TARGET="x86_64-pc-windows-gnu"
       CC=x86_64-w64-mingw32-gcc
+      CARGO_ENV="CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER=x86_64-w64-mingw32-gcc CC_x86_64_pc_windows_gnu=x86_64-w64-mingw32-gcc"
+
+    elif [[ $ARCH = "amd64" ]] && [[ $OS = "freebsd" ]]; then
+      TARGET="x86_64-unknown-freebsd"
+      CC=x86_64-freebsd-cc
+      CARGO_ENV="CARGO_TARGET_X86_64_UNKNOWN_FREEBSD_LINKER=x86_64-freebsd-cc CC_x86_64_unknown_freebsd=x86_64-freebsd-cc"
+
     fi
 
-    env CARGO_TARGET_DIR=. cargo build --release --target=$TARGET --jobs=$CPUCORES
-    mv $TARGET target
+    # Build the Rust static library (libbdkgo.a).
+    # The workspace Cargo.toml at the repo root causes cargo to output to
+    # <repo>/target/<triple>/release/libbdkgo.a.  The CGO LDFLAGS directive
+    # in bdkwallet.go references ${SRCDIR}/../target/release/libbdkgo.a
+    # (Go expands ${SRCDIR} to the bdkwallet/ directory, so this resolves to
+    # <repo>/target/release/libbdkgo.a).  We copy the library there after
+    # the cargo build.
+    # Pass CC to cargo when set so the cc crate uses the right compiler.
+    # CC="$CC" is used as a prefix assignment (not through CARGO_ENV) because
+    # Darwin cross-compile values contain spaces (e.g. "clang -arch x86_64 ...").
+    if [[ -n "$CC" ]]; then
+      CC="$CC" env $CARGO_ENV \
+        cargo build --release \
+        --manifest-path="$REPOROOT/bdkwallet/bdkgo_crate/Cargo.toml" \
+        --target=$TARGET --jobs=$CPUCORES
+    else
+      env $CARGO_ENV \
+        cargo build --release \
+        --manifest-path="$REPOROOT/bdkwallet/bdkgo_crate/Cargo.toml" \
+        --target=$TARGET --jobs=$CPUCORES
+    fi
+
+    mkdir -p "$REPOROOT/target/release"
+    cp "$REPOROOT/target/$TARGET/release/libbdkgo.a" \
+      "$REPOROOT/target/release/"
 
     echo "Building:" $OS $ARCH $ARM
 
     # Build utreexod
-    if [[ $OS == "linux" ]]; then
-      env CC=$CC CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -tags="bdkwallet" -ldflags="-s -w -extldflags "-static" -buildid=" github.com/utreexo/utreexod
-    elif [[ $OS == "darwin" ]]; then
-      env CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -tags="bdkwallet" -ldflags="-s -w -buildid=" github.com/utreexo/utreexod
+    if [[ $OS = "linux" ]]; then
+      env CC=$CC CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -tags="bdkwallet" -ldflags='-s -w -extldflags "-static" -buildid=' github.com/utreexo/utreexod
+    elif [[ $OS = "darwin" ]]; then
+      env CC="${CC:-clang}" CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -tags="bdkwallet" -ldflags='-s -w -buildid=' github.com/utreexo/utreexod
+    elif [[ $OS = "windows" ]]; then
+      env CC=$CC CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH go build -v -tags="bdkwallet" -ldflags='-s -w -extldflags "-static" -buildid=' github.com/utreexo/utreexod
+    elif [[ $OS = "freebsd" ]]; then
+      env CC=$CC CGO_ENABLED=1 GOOS=$OS GOARCH=$ARCH go build -v -tags="bdkwallet" -ldflags='-s -w -buildid=' github.com/utreexo/utreexod
     fi
 
-    # Remove bdk build things.
-    rm -rf target
-    rm -rf release
+    # Remove the copied library (target-specific artifacts under target/<triple>/
+    # are kept so that proc-macros and build scripts don't need to be rebuilt
+    # for the next target).
+    rm -f "$REPOROOT/target/release/libbdkgo.a"
 
     # Build utreexoctl
     env CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH GOARM=$ARM go build -v -trimpath -ldflags="-s -w -buildid=" github.com/utreexo/utreexod/cmd/utreexoctl
@@ -144,5 +204,8 @@ for i in $SYS; do
 
     rm -r $PACKAGE-$i-$TAG
 done
+
+# Clean up cargo build cache.
+rm -rf "$REPOROOT/target"
 
 shasum -a 256 * > manifest-$TAG.txt
